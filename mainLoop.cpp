3f102116e2d3c3f60cf08ce6e61d5a7b3f876968
Jacob Paton <jacobpaton@shaw.ca>
2018-01-27 16:19:08 -0700

    Added input functionality

--- mainLoop.cpp
+++ mainLoop.cpp
@@ -17,16 +17,29 @@ void mainRun() {
     gameInit();
     //Event handler
     SDL_Event e;
+
+    SDL_StartTextInput();
+    std::string text = "";
+
     while (!quit) {
         //Handle events on queue
         while( SDL_PollEvent( &e ) != 0 ) {
             //User requests quit
             std::cout << e.type;
-            if( e.type == SDL_QUIT ) {
+            switch (e.type){
+              case SDL_QUIT:
                 quit = true;
-                std::cout << "You made it to quit. ";
+                std::cout<< "You made it to quit";
+                break;
+              case SDL_KEYDOWN:
+              case SDL_TEXTINPUT:
+                if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_BACKSPACE && text.length() > 0)
+                  text = text.substr(0, text.length()-1);
+                else if (e.type == SDL_TEXTINPUT)
+                  text += e.text.text;
                 break;
             }
+            std::cout << text << std::endl;
         }
     }
     std::cout << "You made it out of !quit loop";
