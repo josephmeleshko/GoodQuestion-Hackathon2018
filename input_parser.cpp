8e119c3a1566a7371101d8718583e911d085b552
ArseniyKD <arseniy.kouzmenkov@gmail.com>
2018-01-27 16:58:37 -0700

    Got the trim() function to... well, function

--- input_parser.cpp
+++ input_parser.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <functional>
+#include <cctype>
+#include <locale>
 using namespace std;
 
-int main(){
-    string input = "";
-    getline(cin, input);
+// trim from start
+static inline string &ltrim(string &s) {
+    s.erase(s.begin(), find_if(s.begin(), s.end(),
+            not1(ptr_fun<int, int>(isspace))));
+    return s;
+}
 
-    cout << "Input received, here it is: " << input << endl;
+// trim from end
+static inline string &rtrim(string &s) {
+    s.erase(find_if(s.rbegin(), s.rend(),
+            not1(ptr_fun<int, int>(isspace))).base(), s.end());
+    return s;
+}
+
+// trim from both ends
+static inline string &trim(string &s) {
+    return ltrim(rtrim(s));
+}
+
+string parse_input(){
+    string base_input = "";
+    getline(cin, base_input);
+    base_input = trim(base_input);
+    cout << base_input << endl;
+
+    //if(base_input[0] = )
+
+
+
+
+
+    return base_input;
+}
+
+
+int main(){
+    cout << parse_input();
 
 
 
