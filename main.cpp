9328ab063e1872b47232049133e4d9ddde444026
josephmeleshko <jmeleshk@ualberta.ca>
2018-01-27 13:06:55 -0700

    2 Window Setup

--- main.cpp
+++ main.cpp
@@ -4,28 +4,55 @@
 
 using namespace std;
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int MAIN_SCREEN_WIDTH = 1080;
+const int MAIN_SCREEN_HEIGHT = 720;
+
+const int TERMINAL_WIDTH = 360;
+const int TERMINAL_HEIGHT = 720;
+
+const int FRAME_LOCATION_X = 50;
+const int FRAME_LOCATION_Y = 100;
 
 int main(int argc, const char* argv[]) {
-    cout << "Main" << endl;
-    cout << one() << endl;
 
-    SDL_Init(SDL_INIT_EVERYTHING);
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) { //Attempt INTI and catch errors
+        cout << "SDL_Init Error: " << SDL_GetError() << endl;
+        return 1;
+    }
 
-    SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
+    SDL_Window* mainWindow = SDL_CreateWindow("Main Game",
+                                            FRAME_LOCATION_X,
+                                            FRAME_LOCATION_Y,
+                                            MAIN_SCREEN_WIDTH,
+                                            MAIN_SCREEN_HEIGHT,
+                                            SDL_WINDOW_SHOWN);
 
-    window = SDL_CreateWindow("TEST", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    screenSurface = SDL_GetWindowSurface(window);
+    SDL_Window* terminalWindow = SDL_CreateWindow("Terminal",
+                                            FRAME_LOCATION_X + MAIN_SCREEN_WIDTH,
+                                            FRAME_LOCATION_Y,
+                                            TERMINAL_WIDTH,
+                                            TERMINAL_HEIGHT,
+                                            SDL_WINDOW_SHOWN);
 
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 255, 255));
-    SDL_UpdateWindowSurface(window);
+    SDL_Surface* mainSurface = SDL_GetWindowSurface(mainWindow);
+    SDL_Surface* terminalSurface = SDL_GetWindowSurface(terminalWindow);
 
-    SDL_Delay(2000);
-    SDL_DestroyWindow(window);
+    SDL_Renderer* mainRenderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Renderer* terminalRenderer = SDL_CreateRenderer(terminalWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
+    SDL_UpdateWindowSurface(mainWindow);
+    SDL_UpdateWindowSurface(terminalWindow);
+
+    SDL_FillRect(mainSurface, NULL, SDL_MapRGB(mainSurface->format, 255, 255, 255));
+    SDL_FillRect(terminalSurface, NULL, SDL_MapRGB(terminalSurface->format, 0, 0, 0));
+
+    SDL_UpdateWindowSurface(mainWindow);
+    SDL_UpdateWindowSurface(terminalWindow);
+
+    SDL_Delay(20000);
+
+    SDL_DestroyWindow(mainWindow);
+    SDL_DestroyWindow(terminalWindow);
     SDL_Quit();
 
     return 0;
