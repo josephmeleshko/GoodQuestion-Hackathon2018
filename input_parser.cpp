59276917f3cb1a481eedb5416e5ca6c905960611
ArseniyKD <arseniy.kouzmenkov@gmail.com>
2018-01-27 16:35:49 -0700

    Starting to create the input parser for commands

---
+++ input_parser.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string input = "";
+    getline(cin, input);
+
+    cout << "Input received, here it is: " << input << endl;
+
+
+
+    return 0;
+}
