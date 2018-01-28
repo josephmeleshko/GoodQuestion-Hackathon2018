c2b1ef1e7bc60c51abb0de8f7af21b1296106401
ArseniyKD <arseniy.kouzmenkov@gmail.com>
2018-01-27 19:03:03 -0700

    The full parser even with the seg fault error fixed

--- input_parser.cpp
+++ input_parser.cpp
@@ -31,17 +31,20 @@ bool InGameBuildFunc(string args){
     string temp = args;
     int count = 0;
     while(temp.find(',') != string::npos){
-        output[count] = temp.substr(0, temp.find(','));
+        if(count < 4){
+            output[count] = temp.substr(0, temp.find(','));
+        }
         count++;
         temp = temp.substr(temp.find(',') + 1);
         cout << temp << endl;
     }
-    output[count] = temp;
-    if(count != 3){
+
+    if(count != 3|| count > 3){
         cout << "Wrong amount of arguments for the Build() command";
         return false;
     }
     else{
+        output[count] = temp;
         for(int i = 0; i < 4; i++){
             cout << output[i] << " ";
         }
@@ -60,17 +63,20 @@ bool InGameDestroyFunc(string args){
         string output [2];
         int count = 0;
         while(temp.find(',') != string::npos){
-            output[count] = temp.substr(0, temp.find(','));
+            if(count < 2){
+                output[count] = temp.substr(0, temp.find(','));
+            }
             count++;
             temp = temp.substr(temp.find(',') + 1);
             cout << temp << endl;
         }
-        output[count] = temp;
-        if(count != 1){
+
+        if(count != 1 || count > 1){
             cout << "Wrong amount of arguments for the Destroy() command";
             return false;
         }
         else{
+            output[count] = temp;
             for(int i = 0; i < 2; i++){
                 cout << output[i] << " ";
             }
@@ -79,6 +85,102 @@ bool InGameDestroyFunc(string args){
     }
 
 
+}
+
+bool InGameChangeLevelFunc(string arg){
+    string temp = trim(arg);
+    if(temp.length() == 0){
+        cout << "Wrong number of arguments for the Change Level() command";
+        return false;
+    }
+    cout << temp << endl;
+    return true;
+}
+
+bool InGameChangeSettingFunc(string args){
+    string temp = args;
+    string output [2];
+    int count = 0;
+    while(temp.find(',') != string::npos ){
+        if(count < 2){
+            output[count] = temp.substr(0, temp.find(','));
+        }
+        count++;
+        temp = temp.substr(temp.find(',') + 1);
+        cout << temp << endl;
+    }
+
+    if(count != 1|| count > 1){
+        cout << "Wrong amount of arguments for the Change Setting() command";
+        return false;
+    }
+    else{
+        output[count] = temp;
+        for(int i = 0; i < 2; i++){
+            cout << output[i] << " ";
+        }
+    }
+    return true;
+}
+
+bool InGameChangeSpeedFunc(string arg){
+    string temp = trim(arg);
+    if(temp.length() == 0){
+        cout << "Wrong number of arguments for the Change Speed() command";
+        return false;
+    }
+    cout << temp << endl;
+    return true;
+}
+
+
+bool InGameHelpFunc(){
+    return true;
+}
+
+
+bool InGameListLevelsFunc(){
+    return true;
+}
+
+
+bool InGameSettingsFunc(){
+    return true;
+}
+
+
+bool InGameCheckResourcesFunc(){
+    return true;
+}
+
+
+bool InGamePauseFunc(){
+    return true;
+}
+
+
+bool InGameContinueFunc(){
+    return true;
+}
+
+
+bool InGameResetFunc(){
+    return true;
+}
+
+
+bool InGameStartFunc(){
+    return true;
+}
+
+
+bool InGameQuitFunc(){
+    return true;
+}
+
+
+bool InGameSaveFunc(){
+    return true;
 }
 
 
@@ -110,13 +212,97 @@ void parse_input(){
             InGameDestroyFunc(arguments);
         }
     }
+    else if(base_input.find("Change Level") != string::npos){
+        if(base_input.find('(') == string::npos || base_input.find(')') == string::npos){
+            string error = "Brackets on both ends are needed to get entire input.";
+            //return error;
+            cout << error;
+        }
+        else{
+            string arguments = base_input.substr(base_input.find('(') + 1, base_input.find(')') - base_input.find('(') - 1);
+            cout << arguments;
+            InGameChangeLevelFunc(arguments);
+        }
+    }
+    else if(base_input.find("Change Setting") != string::npos){
+        if(base_input.find('(') == string::npos || base_input.find(')') == string::npos){
+            string error = "Brackets on both ends are needed to get entire input.";
+            //return error;
+            cout << error;
+        }
+        else{
+            string arguments = base_input.substr(base_input.find('(') + 1, base_input.find(')') - base_input.find('(') - 1);
+            cout << arguments;
+            InGameChangeSettingFunc(arguments);
+        }
+    }
+    else if(base_input.find("Change Speed") != string::npos){
+        if(base_input.find('(') == string::npos || base_input.find(')') == string::npos){
+            string error = "Brackets on both ends are needed to get entire input.";
+            //return error;
+            cout << error;
+        }
+        else{
+            string arguments = base_input.substr(base_input.find('(') + 1, base_input.find(')') - base_input.find('(') - 1);
+            cout << arguments;
+            InGameChangeSpeedFunc(arguments);
+        }
+    }
+    else if(base_input.find("Save") != string::npos){
+        cout << "Entering the Save func" << endl;
+        InGameSaveFunc();
+    }
+    else if(base_input.find("Help") != string::npos){
+        cout << "Entering the Help func" << endl;
+        InGameHelpFunc();
+    }
+    else if(base_input.find("List Levels") != string::npos){
+        cout << "Entering the List Levels func" << endl;
+        InGameListLevelsFunc();
+    }
+    else if(base_input.find("Settings") != string::npos){
+        cout << "Entering the Settings func" << endl;
+        InGameSettingsFunc();
+    }
+    else if(base_input.find("Check Resources") != string::npos){
+        cout << "Entering the Check Resources func" << endl;
+        InGameCheckResourcesFunc();
+    }
+    else if(base_input.find("Pause") != string::npos){
+        cout << "Entering the Pause func" << endl;
+        InGamePauseFunc();
+    }
+    else if(base_input.find("Continue") != string::npos){
+        cout << "Entering the Continue func" << endl;
+        InGameContinueFunc();
+    }
+    else if(base_input.find("Reset") != string::npos){
+        cout << "Entering the Reset func" << endl;
+        InGameResetFunc();
+    }
+    else if(base_input.find("Start") != string::npos){
+        cout << "Entering the Start func" << endl;
+        InGameStartFunc();
+    }
+    else if(base_input.find("Quit") != string::npos){
+        cout << "Entering the Quit func" << endl;
+        InGameQuitFunc();
+    }
+    else{
+        cout << "Wrong Input!" << endl;
+    }
 
 }
 
 
 int main(){
-    parse_input();
-    cout << endl;
+    string input = "";
+    getline(cin, input);
+    while(input != "END"){
+        parse_input();
+        cout << endl;
+        getline(cin, input);
+    }
 
 
 
