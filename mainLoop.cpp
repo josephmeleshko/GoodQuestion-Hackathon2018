61bc779038abb9465d1fb12b7182618eb51e522a
Patrisha-de-Boon <32069658+Patrisha-de-Boon@users.noreply.github.com>
2018-01-27 17:29:32 -0700

    Added button click for editor/terminal

--- mainLoop.cpp
+++ mainLoop.cpp
@@ -26,22 +26,41 @@ void mainRun() {
         while( SDL_PollEvent( &e ) != 0 ) {
             //User requests quit
             switch (e.type){
-              case SDL_QUIT:
-                quit = true;
-                std::cout<< "You made it to quit";
-                break;
-              case SDL_KEYDOWN:
-              case SDL_TEXTINPUT:
-                if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_BACKSPACE && text.length() > 0)
-                  text = text.substr(0, text.length()-1);
-                else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RETURN && text.length() > 0)
-                    text = "";
-                else if (e.type == SDL_TEXTINPUT)
-                  text += e.text.text;
-                break;
+                case SDL_QUIT:
+                    quit = true;
+                    //std::cout<< "You made it to quit";
+                    break;
+                case SDL_KEYDOWN:
+                case SDL_MOUSEBUTTONDOWN:
+                    switch (e.button.button) {
+                        case SDL_BUTTON_LEFT:
+                            int x = e.button.x;
+                            int y = e.button.y;
+
+                            //If the mouse is over the button
+                            if( ( x > 0 ) && ( x < 180 ) && ( y > 0 ) && ( y < 20 ) ) {
+                                //Set the button sprite
+                                //clip = &clips[ CLIP_MOUSEDOWN ];
+                                std::cout << "editor" << std::endl;
+                            }
+                            else if( ( x > 180 ) && ( x < 360 ) && ( y > 0 ) && ( y < 20 ) ) {
+                                //Set the button sprite
+                                //clip = &clips[ CLIP_MOUSEDOWN ];
+                                std::cout << "terminal" << std::endl;
+                            }
+                        }
+                        break;
+                    case SDL_TEXTINPUT:
+                        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_BACKSPACE && text.length() > 0)
+                            text = text.substr(0, text.length()-1);
+                        else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RETURN && text.length() > 0)
+                            text = "";
+                        else if (e.type == SDL_TEXTINPUT)
+                        text += e.text.text;
+                        break;
             }
-            std::cout << text << std::endl;
+            //std::cout << text << std::endl;
         }
     }
-    std::cout << "You made it out of !quit loop";
+    //std::cout << "You made it out of !quit loop";
 }
