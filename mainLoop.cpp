97f76f80690d01a8e388ab8a346618bc156c88d4
Patrisha-de-Boon <32069658+Patrisha-de-Boon@users.noreply.github.com>
2018-01-28 00:44:06 -0700

    Tabbing changes

--- mainLoop.cpp
+++ mainLoop.cpp
@@ -45,18 +45,20 @@ void mainRun() {
             switch (e.type){
                 case SDL_QUIT:
                 case SDL_WINDOWEVENT_CLOSE:
-                    quit = true;
-                    //std::cout<< "You made it to quit";
-                    break;
+                quit = true;
+                //std::cout<< "You made it to quit";
+                break;
+                
                 case SDL_WINDOWEVENT:
                     if (e.window.windowID == SDL_GetWindowID(terminalWindow)){
                         switch(e.window.event){
                             case SDL_WINDOWEVENT_FOCUS_GAINED:
-                                terminalFocus = true;
-                                break;
+                            terminalFocus = true;
+                            break;
+
                             case SDL_WINDOWEVENT_FOCUS_LOST:
-                                terminalFocus = false;
-                                break;
+                            terminalFocus = false;
+                            break;
                         }
                     }
                     break;
@@ -64,50 +66,50 @@ void mainRun() {
                     if (terminalFocus) {
                         switch (e.button.button) {
                             case SDL_BUTTON_LEFT:
-                                int x = e.button.x;
-                                int y = e.button.y;
+                            int x = e.button.x;
+                            int y = e.button.y;
 
-                                //If the mouse is over the button
-                                if( ( x > 0 ) && ( x < 180 ) && ( y > 0 ) && ( y < 40 ) ) {
-                                    //Set the button sprite
-                                    //clip = &clips[ CLIP_MOUSEDOWN ];
-                                    std::cout << "editor" << std::endl;
-                                }
-                                else if( ( x > 180 ) && ( x < 360 ) && ( y > 0 ) && ( y < 40 ) ) {
-                                    //Set the button sprite
-                                    //clip = &clips[ CLIP_MOUSEDOWN ];
-                                    std::cout << "terminal" << std::endl;
-                                }
+                            //If the mouse is over the button
+                            if( ( x > 0 ) && ( x < 180 ) && ( y > 0 ) && ( y < 40 ) ) {
+                                //Set the button sprite
+                                //clip = &clips[ CLIP_MOUSEDOWN ];
+
+                                std::cout << "editor" << std::endl;
+                            }
+                            else if( ( x > 180 ) && ( x < 360 ) && ( y > 0 ) && ( y < 40 ) ) {
+                                //Set the button sprite
+                                //clip = &clips[ CLIP_MOUSEDOWN ];
+                                std::cout << "terminal" << std::endl;
                             }
                         }
-                        break;
-                case SDL_TEXTINPUT:
-                    case SDL_KEYDOWN:
-                        //Handle backspace
-                        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_BACKSPACE && text.length() > 0) {
-                            text.pop_back();
-                        }
-                        //Handle Return
-                        else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RETURN && text.length() > 0) {
-                            text += '\n';
-                        }
-                        //Handle text
-                        if (e.type == SDL_TEXTINPUT) {
-                        text += e.text.text;
+                    }
+                break;
 
-                        }
-                    break;
+                case SDL_TEXTINPUT:
+                case SDL_KEYDOWN:
+                //Handle backspace
+                if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_BACKSPACE && text.length() > 0) {
+                    text.pop_back();
+                }
+                //Handle Return
+                else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RETURN && text.length() > 0) {
+                    text += '\n';
+                }
+                //Handle text
+                if (e.type == SDL_TEXTINPUT) {
+                text += e.text.text;
+
+                }
+                break;
             }
         std::ofstream file("input.txt");
         if (!file) {
             std::cerr << "can't open output file" << std::endl;
         }
         file << text;
-        //std::cout << text << std::flush;
         file.flush();
         file.close();
         }
     }
     SDL_StopTextInput();
-    //std::cout << "You made it out of !quit loop";
 }
